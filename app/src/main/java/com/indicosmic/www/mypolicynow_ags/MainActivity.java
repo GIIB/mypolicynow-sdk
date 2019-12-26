81622254794f665921ccd011f139b17f53f07e94
Akshay <akkikulshrestha28@gmail.com>
2019-12-26 14:35:29 +0530

    Removed Back Button and toolbar sas

--- app/src/main/java/com/indicosmic/www/mypolicynow_ags/MainActivity.java
+++ app/src/main/java/com/indicosmic/www/mypolicynow_ags/MainActivity.java
@@ -2,7 +2,12 @@ package com.indicosmic.www.mypolicynow_ags;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.indicosmic.www.mypolicynow_sdk.QuotationActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView BtnBuyInsurance = (TextView)findViewById(R.id.BtnBuyInsurance);
+        BtnBuyInsurance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), QuotationActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
     }
 }
